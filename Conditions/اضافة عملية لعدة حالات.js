14e92e2f70c8991cdc36c35f03af663fe8e5b0f1
Rayan M <tu4101604@taibahu.edu.sa>
2023-05-21 14:35:06 -0700

    اضافة عملية واحدة لعدة عمليات

---
+++ Conditions/اضافة عملية لعدة حالات.js
@@ -0,0 +1,22 @@
+const colors = ['red', 'green', 'blue'];
+let colorNumber = 5;
+
+switch (colorNumber){
+    case 1:
+        console.log(colors[0]);
+        break;
+    case 2:
+        console.log(colors[1]);
+        break;
+    case 3:
+        console.log(colors[2]);
+        break;
+    case 4:
+    case 5:
+    case 6:
+        console.log('black');
+        break;
+    default:
+        console.log('white');
+        break;
+}
