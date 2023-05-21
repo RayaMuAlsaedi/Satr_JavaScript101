df66c460ec3816a4f9d4c61b31ba2ea0eb61cf4e
Rayan M <tu4101604@taibahu.edu.sa>
2023-05-21 14:32:35 -0700

    الحالة الافتراضية

---
+++ Conditions/DefaultCase.js
@@ -0,0 +1,17 @@
+const colors = ['red', 'green', 'blue'];
+let colorNumber = 55;
+
+switch (colorNumber){
+    case 1:
+        console.log(colors[0]);
+        break;
+    case 2:
+        console.log(colors[1]);
+        break;
+    case 3:
+        console.log(colors[2]);
+        break;
+    default:
+        console.log('white');
+        break;
+}
