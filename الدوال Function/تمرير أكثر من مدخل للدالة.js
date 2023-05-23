e3d078bf0537d64bbb74d7df3304654aeaaa438c
Rayan M <tu4101604@taibahu.edu.sa>
2023-05-23 13:39:54 -0700

    تمرير أكثر من مدخل للدالة

---
+++ الدوال Function/تمرير أكثر من مدخل للدالة.js
@@ -0,0 +1,17 @@
+
+function printNumbers(){
+
+    for (let i =1; i<=3; i++) {
+      console.log(i);
+    }
+  
+  }
+
+  
+  function add (firstNumber, SecondNumber) {
+    console.log(firstNumber + SecondNumber);
+  }
+
+
+add(20, 21);
+add(30,30);
