8268808f7df7ac68a79f9f710ab25d45708e1f07
Rayan M <tu4101604@taibahu.edu.sa>
2023-05-23 14:43:47 -0700

    ارجاع القيمةب استخدام Return

---
+++ الدوال Function/return.js
@@ -0,0 +1,17 @@
+function printNumbers(to){
+
+    for (let i =1; i<=to; i++) {
+      console.log(i);
+    }
+  
+  }
+
+  
+  function add (firstNumber, SecondNumber) {
+    let result = firstNumber + SecondNumber;
+    return result;
+  }
+
+let value = add(2,7);
+
+console.log(value);
