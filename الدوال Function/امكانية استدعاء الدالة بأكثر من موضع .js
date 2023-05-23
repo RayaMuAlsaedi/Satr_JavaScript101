0789d369fd87054014ca797deaeda03c8ff85248
Rayan M <tu4101604@taibahu.edu.sa>
2023-05-23 14:45:41 -0700

    init

---
+++ الدوال Function/امكانية استدعاء الدالة بأكثر من موضع .js
@@ -0,0 +1,17 @@
+function printNumbers(to){
+
+    for (let i =1; i<=to; i++) {
+      console.log(i);
+    }
+  
+  }
+
+  
+  function add (firstNumber, SecondNumber) {
+    let result = firstNumber + SecondNumber;
+    return result;
+  }
+
+let value = add(2,7) - add(2,3);
+
+console.log(value);
