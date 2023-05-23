818814c6317d971ad4c74de27e5a9a779b8a944a
Rayan M <tu4101604@taibahu.edu.sa>
2023-05-23 15:13:51 -0700

    init

---
+++ الدوال Function/التعرف أكثر على Return.js
@@ -0,0 +1,16 @@
+function printNumbers(to){
+  
+    for (let i =1; i<=to; i++) {
+      console.log(i);
+    }
+
+    return;
+  }
+
+  
+  function add (firstNumber, SecondNumber) {
+    return firstNumber + SecondNumber;
+  }
+
+
+printNumbers(5)
