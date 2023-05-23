10bd5e39bf20d238137f09967e40ae33a2e80a4b
Rayan M <tu4101604@taibahu.edu.sa>
2023-05-23 13:41:37 -0700

    تمرير قيمة غير متوقعة

---
+++ الدوال Function/تمرير قيمة غير متوقعة .js
@@ -0,0 +1,16 @@
+function printNumbers(){
+
+    for (let i =1; i<=3; i++) {
+      console.log(i);
+    }
+  
+  }
+
+  
+  function add (firstNumber, SecondNumber) {
+    console.log(firstNumber + SecondNumber);
+  }
+
+
+add("welcome to ", "Javascript");
+add(30,30);
