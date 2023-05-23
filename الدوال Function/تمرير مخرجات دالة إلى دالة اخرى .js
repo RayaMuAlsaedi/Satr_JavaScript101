0fe33388b0869b34493c4cb429ce730bc429254e
Rayan M <tu4101604@taibahu.edu.sa>
2023-05-23 14:48:07 -0700

    init

---
+++ الدوال Function/تمرير مخرجات دالة إلى دالة اخرى .js
@@ -0,0 +1,15 @@
+function printNumbers(to){
+
+    for (let i =1; i<=to; i++) {
+      console.log(i);
+    }
+  
+  }
+
+  
+  function add (firstNumber, SecondNumber) {
+    return firstNumber + SecondNumber;
+  }
+
+
+printNumbers(add(2,7));
